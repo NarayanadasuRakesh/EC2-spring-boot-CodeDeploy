a2e6e9f9e7779e7ca71f99ed4ff1ad1fa43a249d
NarayanadasuRakesh <126420803+NarayanadasuRakesh@users.noreply.github.com>
2024-06-05 12:41:49 +0530

    removed sudo

--- scripts/privileges.sh
+++ scripts/privileges.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-sudo chmod +x /home/ec2-user/java* jar
-sudo chmod +x /home/ec2-user/java/server_start.sh
-sudo chmod +x /home/ec2-user/java/server_stop.sh
+chmod +x /home/ec2-user/java* jar
+chmod +x /home/ec2-user/java/server_start.sh
+chmod +x /home/ec2-user/java/server_stop.sh
