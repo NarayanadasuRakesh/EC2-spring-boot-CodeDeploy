846985766b8a709d101732a1853a46685ea918ff
NarayanadasuRakesh <126420803+NarayanadasuRakesh@users.noreply.github.com>
2024-06-19 14:20:56 +0530

    Updated path

--- scripts/privileges.sh
+++ scripts/privileges.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-chmod +x /home/ec2-user/java-hello-world-with-maven/scripts/server_start.sh
-chmod +x /home/ec2-user/java-hello-world-with-maven/scripts/server_stop.sh
-chmod +x /home/ec2-user/java-hello-world-with-maven/scripts/server_clear.sh
+chmod +x /home/ec2-user/java/scripts/server_start.sh
+chmod +x /home/ec2-user/java/scripts/server_stop.sh
+chmod +x /home/ec2-user/java/scripts/server_clear.sh
